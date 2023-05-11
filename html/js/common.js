b0dcbcdb1872eff81345e1002986e0ec08d1f5e9
DESKTOP-NN9E8HI\학생 <sf27289744@gmail.com>
2023-05-11 18:00:32 +0900

    no message

---
+++ html/js/common.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function () {
+    setTimeout(this.scrollTo, 0, 0, 0, 1);
+}, false);
+
+window.addEventListener("orientationchange", function () {
+    setTimeout(this.scrollTo, 0, 0, 0, 1);
+}, false);
